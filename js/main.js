3851351c06ca78fcdaecfc569394a769a0950c30
Elena Ackovska <elena.ackovska24@gmail.com>
2021-02-01 10:16:49 +0100

    Added audi-clone project

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//jshint esversion:6
+
+const button = document.querrySelector('.navbar-toggler');
+const shoecase = document.querrySelector('.showcase');
+
+button.addEventListener('click', () => {
+showcase.style.background = 'rgba(0,0,0,0.5)';
+});
